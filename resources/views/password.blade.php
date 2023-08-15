16a19c03e25779538a061a613c0ed3e3a47167d8
NgoKhanhChi <126533412+NgoKhanhChi@users.noreply.github.com>
2023-08-15 22:00:41 +0700

    kchi commit 5

--- resources/views/password.blade.php
+++ resources/views/password.blade.php
@@ -3,5 +3,5 @@
     <input type="password" name="current_password" placeholder="Current password">
     <input type="password" name="new_password" placeholder="New password">
     <input type="password" name="new_password_confirmation" placeholder="Confirm new password">
-    <button type="submit">Change password</button>
+    <button type="submit">Change Password</button>
 </form>
